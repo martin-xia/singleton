a5a44b7104a1556c8cf83c5b5a1e1c300b3622ef
martin.xia <martin.xia@servicenow.com>
2015-07-19 20:37:00 -0700

    Add comments

--- sampleInterface.java
+++ sampleInterface.java
@@ -1,5 +1,10 @@
 /*
  * Interface to define functions for service
+ *
+ * Since we don't have worry about double-checked blocking, 
+ * the interface class, service class, other instantiator 
+ * class can be in the same package
+ *
  * Martin.Xia
  */
 public interface sampleInterface {
--- sampleService.java
+++ sampleService.java
@@ -1,5 +1,10 @@
 /*
  * singleton using enum and implements interface
+ *
+ * Since we don't have worry about double-checked blocking, 
+ * the interface class, service class, other instantiator 
+ * class can be in the same package
+ *
  * Martin.Xia
  */
 public enum sampleService implements sampleInterface {
